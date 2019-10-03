e23ca17e629f977d21dfbb72b961323e18c32901
migben <miguelangelben1990@gmail.com>
2019-10-03 01:05:35 -0400

    debugged missing jsx brackets on searchbox

--- src/CardList.js
+++ src/CardList.js
@@ -2,21 +2,23 @@ import React from 'react';
 import Card from './Card';
 
 
-const CardList = ({robots}) => {
-    const cardsArr = robots.map(( user, i) => {
-        return (< Card 
-            key={i} 
-            id={robots[i].id} 
-            name={robots[i].name} 
-            email={robots[i].email}
-            />
-            );
-    });
+const CardList = ({ robots }) => {
     return (
-    < div > 
-         { cardsArr }
-    </div>
-    );
+        <div>
+            {
+                robots.map( (user, i) => {
+                    return (
+                        < Card
+                            key={i}
+                            id={robots[i].id}
+                            name={robots[i].name}
+                            email={robots[i].email}
+                        />
+                    )
+                })
+            }
+        </div>
+    )
 }
 
 export default CardList;
--- src/SearchBox.js
+++ src/SearchBox.js
@@ -1,11 +1,15 @@
 import React from 'react';
 
-const SearchBox = (searchfield, searchChange) => {
+const SearchBox = ({ searchfield, searchChange }) => {
     return (
         < div className="pa2">
-            <input className="pa3 ba b--green bg-lightest-blue" type="search" placeholder="search unit" onChange={ searchChange }/>
+            <input 
+                className="pa3 ba b--green bg-lightest-blue" 
+                type="search" 
+                placeholder="search unit" 
+                onChange={ searchChange }/>
         </ div>
-    )
+    );
 }
 
 export default SearchBox;
